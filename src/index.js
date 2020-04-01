04222ed295173560abcd9f2a71b1d13acab6cd10
rafaelmvale <rafael.marques.vale.dev@gmail.com>
2020-04-01 16:34:37 -0300

    Desafio 7 completo

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
+import './config/ReactotronConfig';
+import store from './store';
+
+
+import Routes from './routes';
+import NavigationService from './services/navigation';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <StatusBar barStyle="light-content" />
+      <Routes
+        ref={(navigatorRef) => NavigationService.setNavigator(navigatorRef)}
+      />
+    </Provider>
+  );
+}
